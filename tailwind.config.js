a47c9050a44a1036ce442522d08eeaec1556fade
Matthew Reid <mreid@small-improvements.com>
2024-06-11 12:12:40 +0200

    update code styles and update card decline codes article

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,26 @@ module.exports = {
                 scale: {
                     '-100': '-1',
                 },
+                typography: ({ theme }) => ({
+                    DEFAULT: {
+                        css: {
+                            code: {
+                                backgroundColor: theme('colors.slate.100'),
+                                borderRadius: theme('borderRadius.sm'),
+                                paddingTop: theme('padding[1]'),
+                                paddingRight: theme('padding[1.5]'),
+                                paddingBottom: theme('padding[1]'),
+                                paddingLeft: theme('padding[1.5]'),
+                            },
+                            'code::before': {
+                                content: 'normal',
+                            },
+                            'code::after': {
+                                content: 'normal',
+                            },
+                        },
+                    },
+                }),
             },
         },
     },
